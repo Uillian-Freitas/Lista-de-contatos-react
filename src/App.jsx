c0c4abc8acf106662192b46bd2351cfec90a68dd
Uillian Freitas <freitasuillian@gmail.com>
2026-03-20 04:29:37 -0300

    alterna texto do botão entre Adicionar/Atualizar e limpa estado após ação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Contato from "./components/contatos/contato"
+import Contato from "./components/contatos/Contato"
 import { useState } from "react"
 
 function App() {
 
-  const [contatos, setContato] = useState ([
+  const [contatos, setContatos] = useState ([
+
     {
       nome: "João silva",
       email: "joao@email.com",
@@ -22,16 +23,82 @@ function App() {
 
   ])
 
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [telefone, setTelefone] = useState('')
+
+    function removerContato(indexParaRemover) {
+      const novaLista = contatos.filter((_, index) => index !== indexParaRemover)
+      setContatos(novaLista)
+    }
+
+    const [indexEditando, setIndexEditando] = useState(null)
+
+    function editarContato(index) {
+      const contato = contatos[index]
+
+      setNome(contato.nome)
+      setEmail(contato.email)
+      setTelefone(contato.telefone)
+
+      setIndexEditando(index)
+    }
+
+    function salvarContato() {
+      const novoContato = {
+        nome,
+        email,
+        telefone
+      }
+
+      if (indexEditando === null) {
+        setContatos([...contatos, novoContato])
+      }else {
+  const novaLista = contatos.map((contato, index) => {
+    if(index === indexEditando) {
+      return novoContato
+    }
+    return contato
+  })
+  
+  setContatos(novaLista)
+  setIndexEditando(null)
+}
+setNome('')
+setEmail("")
+setTelefone('')
+      }
     return (
     
         <div>
           <h1>Lista de contatos</h1>
+          <input type="text"
+          placeholder="Digite um nome" 
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}/>
+          <p>{nome}</p>
+
+          <input type="text"
+          placeholder="Digite o e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+          <p>{email}</p>
+
+          <input type="text"
+          placeholder="Digite um telefone"
+          value={telefone}
+          onChange={(e) => setTelefone(e.target.value)} />
+          <p>{telefone}</p>
+
+          <button onClick={salvarContato}>{indexEditando === null ? "Adicionar" : "Atualizar"}</button>
         {contatos.map((contato, index) => (
         <Contato
         key={index}
         nome={contato.nome}
         email={contato.email}
         telefone={contato.telefone}
+        onEditar={() => editarContato(index)}
+        onRemover={() => removerContato(index)}
         />
         ))}
         </div>
--- src/components/contatos/contato.jsx
+++ src/components/contatos/contato.jsx
@@ -4,6 +4,10 @@ function Contato(props){
             <h2>{props.nome}</h2>
             <p>{props.email}</p>
             <p>{props.telefone}</p>
+
+            <button onClick={props.onEditar}>Editar</button>
+            <button onClick={props.onRemover}>Remover</button>
+
         </div>
     )
 }
