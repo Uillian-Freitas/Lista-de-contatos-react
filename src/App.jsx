968feadbb2a2fcee2ad8dc13edc7f5ebb3fba562
Uillian Freitas <freitasuillian@gmail.com>
2026-03-21 21:43:18 -0300

    feat: inicia estilização global com styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
 import Contato from "./components/contatos/Contato"
 import { useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+background-color: #1e1e2f;
+color: white;
+
+display: flex;
+justify-content: center;
+align-items: center
+
+min-height: 100vh;
+`
+
+const Input = styled.input`
+padding: 10px;
+margin: 5px;
+border-radius: 5px;
+border: none;
+`
+
+const Botao = styled.button`
+padding: 10px;
+margin: 10px;
+background-color: #4CAF50;
+color: white;
+border: none;
+Border-radius: 5px;
+cursor: pointer;
+`
+
+const Card = styled.div`
+background-color: #1a1a1a;
+padding: 30px;
+border-radius: 10px;
+Width: 400px;
+`
+
+
 
 function App() {
 
@@ -27,9 +65,16 @@ function App() {
     const [email, setEmail] = useState('')
     const [telefone, setTelefone] = useState('')
 
+    const [removendoIndex, setRemovendoIndex] = useState(null)
+
     function removerContato(indexParaRemover) {
-      const novaLista = contatos.filter((_, index) => index !== indexParaRemover)
-      setContatos(novaLista)
+      setRemovendoIndex(indexParaRemover)
+
+      setTimeout(() => {
+        const novaLista = contatos.filter((_, index) => index !== indexParaRemover)
+        setContatos(novaLista)
+        setRemovendoIndex(null)
+      }, 500)
     }
 
     const [indexEditando, setIndexEditando] = useState(null)
@@ -70,27 +115,28 @@ setTelefone('')
       }
     return (
     
-        <div>
+        <Container>
+          <Card>
           <h1>Lista de contatos</h1>
-          <input type="text"
+          <Input type="text"
           placeholder="Digite um nome" 
           value={nome}
           onChange={(e) => setNome(e.target.value)}/>
           <p>{nome}</p>
 
-          <input type="text"
+          <Input type="text"
           placeholder="Digite o e-mail"
           value={email}
           onChange={(e) => setEmail(e.target.value)} />
           <p>{email}</p>
 
-          <input type="text"
+          <Input type="text"
           placeholder="Digite um telefone"
           value={telefone}
           onChange={(e) => setTelefone(e.target.value)} />
           <p>{telefone}</p>
 
-          <button onClick={salvarContato}>{indexEditando === null ? "Adicionar" : "Atualizar"}</button>
+          <Botao onClick={salvarContato}>{indexEditando === null ? "Adicionar" : "Atualizar"}</Botao>
         {contatos.map((contato, index) => (
         <Contato
         key={index}
@@ -98,10 +144,12 @@ setTelefone('')
         email={contato.email}
         telefone={contato.telefone}
         onEditar={() => editarContato(index)}
+        removendo={index === removendoIndex}
         onRemover={() => removerContato(index)}
         />
         ))}
-        </div>
+        </Card>
+        </Container>
   )
 }
 
--- src/components/contatos/contato.jsx
+++ src/components/contatos/contato.jsx
@@ -1,6 +1,20 @@
+import styled from "styled-components"
+
+const Card = styled.div`
+background-color: ${(props) => (props.removendo ? '#ff4d4d' : '#2a2a3d')};
+padding: 15px;
+margin: 10px 0;
+border-radius: 10px;
+transition: all 0.5s ease;
+opacity: ${(props) => (props.removendo ? 0 : 1)};
+transform: ${(props) =>
+    props.removendo ? 'scale(0.8) rotate(5deg)' : 'scale(1)'};
+}
+`
+
 function Contato(props){
     return(
-        <div>
+        <Card removendo={props.removendo}>
             <h2>{props.nome}</h2>
             <p>{props.email}</p>
             <p>{props.telefone}</p>
@@ -8,7 +22,7 @@ function Contato(props){
             <button onClick={props.onEditar}>Editar</button>
             <button onClick={props.onRemover}>Remover</button>
 
-        </div>
+        </Card>
     )
 }
 
